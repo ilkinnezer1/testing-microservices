4c4efc8477d9de1056a056848712cba0026952fc
IFactory <ilkin.nezerov@gmail.com>
2023-05-19 15:57:10 +0400

    Set Up Database Config for Auth Service
    - create db and tables
    - add db connection config for auth service to connect

--- auth-service/server/server.go
+++ auth-service/server/server.go
@@ -4,15 +4,67 @@ import (
 	"authentication/data"
 	"authentication/routes"
 	"database/sql"
+	"log"
+	"os"
+	"time"
 )
+
+var counts int64
 
 type Config struct {
 	DB     *sql.DB
 	Models data.Models
 }
 
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+
+	}
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Database is not ready")
+			counts++
+		} else {
+			log.Println("Connected to Database")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for 2 seconds")
+		time.Sleep(time.Second * 2)
+		continue
+	}
+}
+
 func Start() {
 	e := routes.AuthRoutes()
+
+	conn := connectToDB()
+	if conn == nil {
+		log.Panicln("Can't connect to the postgres")
+	}
+
+	_ = Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	if err := e.Start(":6000"); err != nil {
 		panic(err)
